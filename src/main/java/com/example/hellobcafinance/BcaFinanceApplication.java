9e4069526660ecb5a6ef1f70c3ca9ab16aa1ab4a
amindraa05 <60906676+amindraa05@users.noreply.github.com>
2024-01-30 17:17:20 +0700

    Update BcaFinanceApplication.java

--- src/main/java/com/example/hellobcafinance/BcaFinanceApplication.java
+++ src/main/java/com/example/hellobcafinance/BcaFinanceApplication.java
@@ -2,6 +2,8 @@ package com.example.hellobcafinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BcaFinanceApplication {
